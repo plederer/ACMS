04a189daef5c0ecd32c40ceca019ca47f4258a15
Elena Giammatteo <e.giammatteo@utwente.nl>
2024-01-22 13:18:30 +0100

    Test with new mesh

--- numex/helmholtz_aux.py
+++ numex/helmholtz_aux.py
@@ -26,50 +26,88 @@ import time
 
 def unit_disc(maxH):
     
-    # GEOMETRY
-    geo = SplineGeometry()
-    Points = [(0,-1), (1,-1), (1,0), 
-            (1,1), (0,1), (-1,1),
-            (-1,0), (-1,-1), (0,0)]
+    l = sqrt(2) # Edge of square
+    r = 1 # Circle radius
 
-    bcs_edge = ["c0", "c1", "c2", "c3", 
-                "m0", "m1", "m2", "m3",
-                "m4", "m5", "m6", "m7"]
+    circ = WorkPlane().Circle(0, 0, r).Face()
+    rect = WorkPlane().Rotate(45).RectangleC(l, l).Face()
+    quadUR = MoveTo(r/2,r/2).RectangleC(r, r).Face()
+    quadUL = MoveTo(-r/2,r/2).RectangleC(r, r).Face()
+    quadLR = MoveTo(r/2,-r/2).RectangleC(r, r).Face()
+    quadLL = MoveTo(-r/2,-r/2).RectangleC(r, r).Face()
 
-    for i, pnt in enumerate(Points):
-        geo.AddPoint(*pnt, name = "V" + str(i))
+    triangleUR = rect - quadUR 
+    triangleUL = rect - quadUL
+    triangleLR = rect - quadLR
+    triangleLL = rect - quadLL
 
-    geo.Append(["spline3", 0, 1, 2], leftdomain=1, rightdomain=0, bc="c0")
-    geo.Append(["spline3", 2, 3, 4], leftdomain=2, rightdomain=0, bc="c1")
-    geo.Append(["spline3", 4, 5, 6], leftdomain=3, rightdomain=0, bc="c2")
-    geo.Append(["spline3", 6, 7, 0], leftdomain=4, rightdomain=0, bc="c3")
-    geo.Append(["line", 0, 2], leftdomain=5, rightdomain=1, bc="m0")
-    geo.Append(["line", 2, 4], leftdomain=6, rightdomain=2, bc="m1")
-    geo.Append(["line", 4, 6], leftdomain=7, rightdomain=3, bc="m2")
-    geo.Append(["line", 6, 0], leftdomain=8, rightdomain=4, bc="m3")
+    circ.edges.name = "dom_bnd"
 
-    geo.Append(["line", 8, 0], leftdomain=5, rightdomain=8, bc="m4")
-    geo.Append(["line", 8, 2], leftdomain=6, rightdomain=5, bc="m5")
-    geo.Append(["line", 8, 4], leftdomain=7, rightdomain=6, bc="m6")
-    geo.Append(["line", 8, 6], leftdomain=8, rightdomain=7, bc="m7")
+    outer = circ - rect
 
-    # geo = SplineGeometry()
-    # geo.AddCircle ( (0, 0), r=1, leftdomain=1, rightdomain=0, )
+    shape = Glue([outer, triangleUR, triangleUL, triangleLR, triangleLL])
 
-    ngmesh = geo.GenerateMesh(maxh = maxH)
 
-    mesh = Mesh(ngmesh)
-    mesh.Curve(order = 10)
-    for i in range(8):
-        mesh.ngmesh.SetMaterial(i+1,"om" + str(i))
+    # DrawGeo(shape)
 
-    Draw(mesh)
+    mesh = Mesh(OCCGeometry(shape, dim=2).GenerateMesh(maxh = maxH))
+    mesh.Curve(10)
+    # DrawGeo(mesh)
+
     print(mesh.nv)
-    # print(mesh.GetMaterials())
-    # print(mesh.GetBoundaries())
-    # print(mesh.GetBBoundaries())
+    # print(mesh.GetMaterials()) # 8 subdomains
+    # print(mesh.GetBoundaries()) # 12 edges
+    # print(mesh.GetBBoundaries()) # 5 vertices
 
-    dom_bnd = "c0|c1|c2|c3"
+    dom_bnd = "dom_bnd"
+        
+    
+    
+    
+    # # GEOMETRY
+    # geo = SplineGeometry()
+    # Points = [(0,-1), (1,-1), (1,0), 
+    #         (1,1), (0,1), (-1,1),
+    #         (-1,0), (-1,-1), (0,0)]
+
+    # bcs_edge = ["c0", "c1", "c2", "c3", 
+    #             "m0", "m1", "m2", "m3",
+    #             "m4", "m5", "m6", "m7"]
+
+    # for i, pnt in enumerate(Points):
+    #     geo.AddPoint(*pnt, name = "V" + str(i))
+
+    # geo.Append(["spline3", 0, 1, 2], leftdomain=1, rightdomain=0, bc="c0")
+    # geo.Append(["spline3", 2, 3, 4], leftdomain=2, rightdomain=0, bc="c1")
+    # geo.Append(["spline3", 4, 5, 6], leftdomain=3, rightdomain=0, bc="c2")
+    # geo.Append(["spline3", 6, 7, 0], leftdomain=4, rightdomain=0, bc="c3")
+    # geo.Append(["line", 0, 2], leftdomain=5, rightdomain=1, bc="m0")
+    # geo.Append(["line", 2, 4], leftdomain=6, rightdomain=2, bc="m1")
+    # geo.Append(["line", 4, 6], leftdomain=7, rightdomain=3, bc="m2")
+    # geo.Append(["line", 6, 0], leftdomain=8, rightdomain=4, bc="m3")
+
+    # geo.Append(["line", 8, 0], leftdomain=5, rightdomain=8, bc="m4")
+    # geo.Append(["line", 8, 2], leftdomain=6, rightdomain=5, bc="m5")
+    # geo.Append(["line", 8, 4], leftdomain=7, rightdomain=6, bc="m6")
+    # geo.Append(["line", 8, 6], leftdomain=8, rightdomain=7, bc="m7")
+
+    # # geo = SplineGeometry()
+    # # geo.AddCircle ( (0, 0), r=1, leftdomain=1, rightdomain=0, )
+
+    # ngmesh = geo.GenerateMesh(maxh = maxH)
+
+    # mesh = Mesh(ngmesh)
+    # mesh.Curve(order = 10)
+    # for i in range(8):
+    #     mesh.ngmesh.SetMaterial(i+1,"om" + str(i))
+
+    # Draw(mesh)
+    # print(mesh.nv)
+    # # print(mesh.GetMaterials())
+    # # print(mesh.GetBoundaries())
+    # # print(mesh.GetBBoundaries())
+
+    # dom_bnd = "c0|c1|c2|c3"
 
     return mesh, dom_bnd
 
