42beaee58896373d4ebcfc4bef25bea7a165c90a
Philip <philip@u24.at>
2024-09-04 16:29:55 +0200

    save fig for transition

--- numex/transition.py
+++ numex/transition.py
@@ -1,33 +1,24 @@
 # LIBRARIES
 from helmholtz_aux import *
+import matplotlib.pyplot as plt
 
-do_draw = False
+do_draw = True
 if do_draw:
     import netgen.gui
 from ngsolve.eigenvalues import PINVIT
 
 
 problem = 5
-Ncell = 18
+Ncell = 14
 incl = 2
 
-omega_v = [1] 
+omega = 1
 
 Href = 0
 maxH = 0.1
-order_v = [3]
-Bubble_modes = [0]
-Edge_modes = [4]
-ACMS_flag = 0
+order = 3
+EE = 4
 
-Bubble_modes = [0]
-
-error_table = 1
-table_content_l2_aux = ""
-table_content_h1_aux = ""
-table_header = ""
-table_separation = ""
-table_end = ""
 
 
 r  = 0.126     # radius of inclusion
@@ -39,11 +30,9 @@ alpha_outer = 1/12.1 #SILICON
 alpha_inner = 1 #0 #AIR             
 layers = 0
 
-wg = 3
-
 ix = [i for i in range(layers)] + [Nx - 1 - i for i in range(layers)]
-# iy = [Ncell//(2*incl)] #
-iy = [i for i in range(layers)] + [Ny - 1 - i for i in range(layers)]
+iy = [Ncell//(2*incl)] #
+# iy = [i for i in range(layers)] + [Ny - 1 - i for i in range(layers)]
 
 defects = np.ones((Nx,Ny))
 for i in ix:
@@ -54,18 +43,13 @@ for j in iy:
     for i in range(Nx): 
         defects[i,j] = 0.0 
 
-load_mesh = True
-mesh, dom_bnd, alpha, mesh_info = crystal_geometry(maxH, Nx, Ny, incl, r, Lx, Ly, alpha_outer, alpha_inner, defects, layers, load_mesh)
-V = H1(mesh, order = order_v[0], complex = True)
+mesh, dom_bnd, alpha, mesh_info = crystal_geometry(maxH, Nx, Ny, incl, r, Lx, Ly, alpha_outer, alpha_inner, defects, layers, load_mesh = True)
 
-# input()
-# print(mesh.Get)
 
-ints_right = []
-ints_left = []
-# SetNumThreads(12)
+save_fig = True
+calc_fem = False
+calc_acms = True
 
-omega = omega_v[0]
 if True:
     print("omega = ", omega)
     
@@ -74,7 +58,7 @@ if True:
     k = k_ext * CF((1,0)) #CF = CoefficientFunction
     beta = - k_ext / omega
     f = 0 
-    sigma = 1
+    sigma = 2
     off = Ncell/2 - 0.5 * incl
     peak = exp(-(y-off)**2*sigma)
     # print("off = ", off)
@@ -87,10 +71,10 @@ if True:
     gamma = 1
     
     # solution_dictionary = ground_truth(mesh, variables_dictionary, 10)
-    if True:
+    if calc_fem:
         start = time.time()
         
-        V = H1(mesh, order = order_v[0], complex = True) 
+        V = H1(mesh, order = order, complex = True) 
         # print("V.ndof = ", V.ndof)
         u, v = V.TnT()
 
@@ -116,8 +100,7 @@ if True:
 
         Draw(gfu_fem, mesh,"ufem")
 
-    if True:
-
+    if calc_acms:
         ###############################################################
         # reconstruct the acms_solution only on the cells next to the domain boundary on the left and the right side
         domsba = BitArray(len(mesh.GetMaterials()))
@@ -132,7 +115,7 @@ if True:
                 doms.append(mesh.ngmesh.GetMaterial(ii+1))
         ###############################################################
               
-        acms = ACMS(order = order_v[0], mesh = mesh, bm = 0, em = Edge_modes[0], bi = mesh.GetCurveOrder(), mesh_info = mesh_info, alpha = alpha, omega = omega, kappa = kappa, f = f, g = g, beta = beta, gamma = gamma, save_localbasis=doms, save_extensions = doms)
+        acms = ACMS(order = order, mesh = mesh, bm = 0, em = EE, bi = mesh.GetCurveOrder(), mesh_info = mesh_info, alpha = alpha, omega = omega, kappa = kappa, f = f, g = g, beta = beta, gamma = gamma, save_localbasis=doms, save_extensions = doms)
                         
         edge_basis = acms.calc_edge_basis()
         if edge_basis:
@@ -150,41 +133,64 @@ if True:
     if do_draw:
         input()
 
+    # number of cell in upper and lower direction plus waveguide, i.e. N_craw = 2 plots the solution over 5 cells
+    N_draw = 2
 
-N = 100 * Ncell
-off = 0.5 * incl
-xx = np.linspace(-off, Ncell-off, N)
-wave_in = []
-wave_out = []
+    N = 100 * (2*N_draw + 1)
+    center = -0.5 * incl + Ncell/2
+    off = 0.5 * incl
+    
+    xx = np.linspace(center - off - N_draw, center + off + N_draw, N)
+    wave_in = []
+    wave_out = []
 
-acms_wave_in = []
-acms_wave_out = []
-
-
-for xi in xx:
-    val = gfu_fem(mesh(-off, xi))
-    val_out = gfu_fem(mesh(-off+Ncell, xi))
-    acms_val = gfu_acms(mesh(-off, xi))
-    acms_val_out = gfu_acms(mesh(-off+Ncell, xi))
-
-    wave_in.append(sqrt(val.real**2 + val.imag**2))
-    wave_out.append(sqrt(val_out.real**2 + val_out.imag**2))
-
-    acms_wave_in.append(sqrt(acms_val.real**2 + acms_val.imag**2))
-    acms_wave_out.append(sqrt(acms_val_out.real**2 + acms_val_out.imag**2))
-                
-import matplotlib.pyplot as plt
-
-plt.plot(xx,wave_in, label = "left")
-plt.plot(xx,wave_out, label = "out")
-
-plt.plot(xx,acms_wave_in, label = "left_acms")
-plt.plot(xx,acms_wave_out, label = "out_acms")
-
-plt.legend()
-plt.show()
+    acms_wave_in = []
+    acms_wave_out = []
 
 
+    for xi in xx:
+        if calc_fem:
+            val = gfu_fem(mesh(-off, xi))
+            val_out = gfu_fem(mesh(-off+Ncell, xi))
+
+            wave_in.append(sqrt(val.real**2 + val.imag**2))
+            wave_out.append(sqrt(val_out.real**2 + val_out.imag**2))
+
+        if calc_acms:
+            acms_val = gfu_acms(mesh(-off, xi))
+            acms_val_out = gfu_acms(mesh(-off+Ncell, xi))
+
+            acms_wave_in.append(sqrt(acms_val.real**2 + acms_val.imag**2))
+            acms_wave_out.append(sqrt(acms_val_out.real**2 + acms_val_out.imag**2))
+    if calc_fem:    
+        plt.plot(xx,wave_in, label = "left")
+        plt.plot(xx,wave_out, label = "out")
+
+    if calc_acms:
+        plt.plot(xx,acms_wave_in, label = "left_acms")
+        plt.plot(xx,acms_wave_out, label = "out_acms")
+
+    plt.legend()
+
+    if save_fig:
+        if not os.path.exists("transition"):
+            os.mkdir("transition")
+
+        dirname = os.path.dirname(__file__)
+        file_name = "transition_" +  "maxH:" +     str(maxH) + "_" + \
+                                "Ncell:" +    str(Ncell) + "_" + \
+                                "order:" +   str(order) + "_" + \
+                                "Ie:" +      str(EE) + "_" + \
+                                "omega:" +      str(omega) + "_" + \
+                                ".png"
+
+        save_file = os.path.join(dirname, "transition/" +  file_name)
+
+        plt.savefig(save_file, dpi=400)
+    else:
+        plt.show()
 
 
-        
+
+
+    
