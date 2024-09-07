d070564dc333579a536ba52ed465788de52547ce
Philip <philip@u24.at>
2024-09-07 22:03:41 +0200

    transition file

--- numex/transition.py
+++ numex/transition.py
@@ -2,30 +2,36 @@
 from helmholtz_aux import *
 import matplotlib.pyplot as plt
 
-do_draw = True
+do_draw = False
+save_fig = False
+draw_fig = True
+calc_fem = True
+calc_acms = True
+vtk_do = False
+
 if do_draw:
     import netgen.gui
 from ngsolve.eigenvalues import PINVIT
 
 
 problem = 5
-Ncell = 14
+Ncell = 10
 incl = 2
 
-omega = 1
+omega = 1.48 # 1.8288
 
 Href = 0
-maxH = 0.1
-order = 3
-EE = 4
+maxH = 0.2
+order = 4
+EE = 16
 
 
 
-r  = 0.126     # radius of inclusion
+r  = 0.25 #0.126     # radius of inclusion
 Lx = 1 * incl   #* 0.484 #"c"
 Ly = Lx        #0.685 #"a
 Nx = Ncell // incl # number of cells in x direction
-Ny = Nx       # number of cells in y direction
+Ny = Ncell // incl       # number of cells in y direction
 alpha_outer = 1/12.1 #SILICON
 alpha_inner = 1 #0 #AIR             
 layers = 0
@@ -46,9 +52,6 @@ for j in iy:
 mesh, dom_bnd, alpha, mesh_info = crystal_geometry(maxH, Nx, Ny, incl, r, Lx, Ly, alpha_outer, alpha_inner, defects, layers, load_mesh = True)
 
 
-save_fig = True
-calc_fem = False
-calc_acms = True
 
 if True:
     print("omega = ", omega)
@@ -64,7 +67,7 @@ if True:
     # print("off = ", off)
     # Draw(y-off, mesh, "yy")
     g = 1j * (k_ext - k * specialcf.normal(2)) * exp(-1j * (k[0] * x + k[1] * y)) * peak # Incoming plane wave 
-    # Draw(g, mesh, "g")
+    Draw(g, mesh, "g")
     u_ex = 0
     sol_ex = 0
     Du_ex = 0
@@ -113,6 +116,8 @@ if True:
         for ii, d in enumerate(domsba):
             if d == 1:
                 doms.append(mesh.ngmesh.GetMaterial(ii+1))
+        
+        # doms = None #[]
         ###############################################################
               
         acms = ACMS(order = order, mesh = mesh, bm = 0, em = EE, bi = mesh.GetCurveOrder(), mesh_info = mesh_info, alpha = alpha, omega = omega, kappa = kappa, f = f, g = g, beta = beta, gamma = gamma, save_localbasis=doms, save_extensions = doms)
@@ -171,25 +176,57 @@ if True:
         plt.plot(xx,acms_wave_out, label = "out_acms")
 
     plt.legend()
-
+    
+    file_name = "transition_" +  "maxH:" +     str(maxH) + "_" + \
+                                "Ncell:" +    str(Ncell) + "_" + \
+                                "order:" +   str(order) + "_" + \
+                                "Ie:" +      str(EE) + "_" + \
+                                "omega:" +      str(omega) + "_"
+                                
+    
     if save_fig:
         if not os.path.exists("transition"):
             os.mkdir("transition")
 
         dirname = os.path.dirname(__file__)
-        file_name = "transition_" +  "maxH:" +     str(maxH) + "_" + \
-                                "Ncell:" +    str(Ncell) + "_" + \
-                                "order:" +   str(order) + "_" + \
-                                "Ie:" +      str(EE) + "_" + \
-                                "omega:" +      str(omega) + "_" + \
-                                ".png"
+        # file_name = "transition_" +  "maxH:" +     str(maxH) + "_" + \
+        #                         "Ncell:" +    str(Ncell) + "_" + \
+        #                         "order:" +   str(order) + "_" + \
+        #                         "Ie:" +      str(EE) + "_" + \
+        #                         "omega:" +      str(omega) + "_" + \
+        #                         ".png"
 
-        save_file = os.path.join(dirname, "transition/" +  file_name)
+        save_file = os.path.join(dirname, "transition/" +  file_name + ".png")
 
         plt.savefig(save_file, dpi=400)
-    else:
+    if draw_fig:
         plt.show()
 
+    if vtk_do:
+        if not os.path.exists("vtks"):
+            os.mkdir("vtks")
+
+        fields = []
+        field_names = []
+
+        if calc_fem: 
+            # V = VectorH1(mesh, order = order)
+            # gf = GridFunction(V)
+            # gf.Set(CF((gfu_fem.imag,gfu_fem.imag)))
+            
+            fields.append(gfu_fem.imag)
+            fields.append(gfu_fem.real)
+            field_names.append("gfu_fem_imag")
+            field_names.append("gfu_fem_real")
+        if calc_acms: 
+            fields.append(gfu_acms.imag)
+            fields.append(gfu_acms.real)
+            field_names.append("gfu_acms_imag")
+            field_names.append("gfu_acms_real")
+        # print(field_names)
+
+        vtk = VTKOutput(ma=mesh, coefs = fields, names =  field_names, filename="./vtks/" + file_name + ".vtk", subdivision=3)
+        vtk.Do()
 
 
 
